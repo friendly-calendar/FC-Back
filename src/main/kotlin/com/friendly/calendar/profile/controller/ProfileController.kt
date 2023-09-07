59e09fe3a7a072796ddeb0ff38d0cbf7aefc7894
csbiy <katd6@naver.com>
2023-09-07 17:16:42 +0900

    profile업로드 API 틀, 디렉토리 구조 설정

---
+++ src/main/kotlin/com/friendly/calendar/profile/controller/ProfileController.kt
@@ -0,0 +1,24 @@
+package com.friendly.calendar.profile.controller
+
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/profile")
+class ProfileController {
+
+    @GetMapping
+    fun getProfile(): String {
+        TODO("not implemented")
+    }
+
+    @PostMapping
+    fun createOrUpdateProfile(): String {
+        TODO("not implemented")
+    }
+
+    @DeleteMapping
+    fun deleteProfile(): String {
+        TODO("not implemented")
+    }
+
+}
