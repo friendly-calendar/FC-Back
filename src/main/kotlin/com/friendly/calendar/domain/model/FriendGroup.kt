8469eb9f0bcc1e99e87463ce74f07c8a798012c9
jylim <jylim@crscube.co.kr>
2023-09-08 10:45:13 +0900

    Add GriendGroup entity

---
+++ src/main/kotlin/com/friendly/calendar/domain/model/FriendGroup.kt
@@ -0,0 +1,24 @@
+package com.friendly.calendar.domain.model
+
+import com.friendly.calendar.entity.baseEntity.BaseEntity
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class FriendGroup(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "grp_key")
+    val id: Long = 0,
+
+    @Column(length = 40)
+    val label: String,
+
+    @Column(length = 1000)
+    val description: String,
+
+    val imageUrl: String,
+) : BaseEntity()
