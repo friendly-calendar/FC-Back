5a9cb7fbe917ca54d99742710b6de897c7577c6b
jylim <jylim@crscube.co.kr>
2023-09-12 17:00:17 +0900

    Add friend status service

---
+++ src/main/kotlin/com/friendly/calendar/domain/service/FriendStatusService.kt
@@ -0,0 +1,18 @@
+package com.friendly.calendar.domain.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class FriendStatusService {
+    fun requestFriend(userKey: Long): String {
+        TODO("not implemented")
+    }
+
+    fun acceptFriend(userKey: Long): String {
+        TODO("not implemented")
+    }
+
+    fun rejectFriend(userKey: Long): String {
+        TODO("not implemented")
+    }
+}
