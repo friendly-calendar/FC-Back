dc1702cf7c71f4d837b483169e25dfefac6434e6
dokdogalmaegi <clik202012@gmail.com>
2023-09-12 21:01:14 +0900

    Add requestFriend service function

--- src/main/kotlin/com/friendly/calendar/domain/service/FriendStatusService.kt
+++ src/main/kotlin/com/friendly/calendar/domain/service/FriendStatusService.kt
@@ -1,11 +1,34 @@
 package com.friendly.calendar.domain.service
 
+import com.friendly.calendar.domain.model.FriendRequest
+import com.friendly.calendar.domain.model.User
+import com.friendly.calendar.domain.model.enum.FriendLogStatus
+import com.friendly.calendar.domain.persistence.FriendRequestRepository
+import com.friendly.calendar.repository.UserRepository
 import org.springframework.stereotype.Service
+import java.util.Optional
 
 @Service
-class FriendStatusService {
-    fun requestFriend(userKey: Long): String {
-        TODO("not implemented")
+class FriendStatusService(
+    private val friendRequestRepository: FriendRequestRepository,
+    private val userRepository: UserRepository
+) {
+    fun requestFriend(senderKey: Long, receiverKey: Long, requestMessage: String): Unit {
+        val sender: Optional<User> = userRepository.findById(senderKey)
+        val receiver: Optional<User> = userRepository.findById(receiverKey)
+
+        val friendRequest: FriendRequest = sender.map { sender ->
+            receiver.map { receiver ->
+                FriendRequest(
+                    sender = sender,
+                    receiver = receiver,
+                    status = FriendLogStatus.PENDING,
+                    message = requestMessage
+                )
+            }.orElseThrow { IllegalArgumentException("receiver not found") }
+        }.orElseThrow { IllegalArgumentException("sender not found") }
+
+        friendRequestRepository.save(friendRequest)
     }
 
     fun acceptFriend(userKey: Long): String {
