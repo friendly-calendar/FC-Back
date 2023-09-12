1b191c4547045f650307d37fadbe026045c6cbfe
jylim <jylim@crscube.co.kr>
2023-09-12 16:37:17 +0900

    Add request friend controller

--- src/main/kotlin/com/friendly/calendar/controller/FriendController.kt
+++ src/main/kotlin/com/friendly/calendar/controller/FriendController.kt
@@ -3,7 +3,10 @@ package com.friendly.calendar.controller
 import com.friendly.calendar.domain.model.FriendRelation
 import com.friendly.calendar.domain.service.FriendService
 import com.friendly.calendar.network.ResponseDto
+import com.friendly.calendar.network.friend.FriendRequestDto
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -17,5 +20,10 @@ class FriendController(private val friendService: FriendService) {
         val friendList: List<FriendRelation> = friendService.getFriends(userKey);
 
         return ResponseDto.success(data = friendList)
+    }
+
+    @PostMapping
+    fun requestFriend(@RequestBody friendRequestDto: FriendRequestDto): String? {
+        TODO("not implemented")
     }
 }
