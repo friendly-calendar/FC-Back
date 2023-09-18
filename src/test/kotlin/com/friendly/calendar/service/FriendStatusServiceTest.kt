1dcf247707dfa43369ce3257a08f9feb3d038c57
jylim <jylim@crscube.co.kr>
2023-09-18 20:17:47 +0900

    Change test

--- src/test/kotlin/com/friendly/calendar/service/FriendStatusServiceTest.kt
+++ src/test/kotlin/com/friendly/calendar/service/FriendStatusServiceTest.kt
@@ -1,9 +1,12 @@
 package com.friendly.calendar.service
 
 import com.friendly.calendar.domain.model.User
+import com.friendly.calendar.domain.persistence.FriendRelationRepository
 import com.friendly.calendar.domain.persistence.FriendRequestRepository
 import com.friendly.calendar.domain.persistence.UserRepository
+import com.friendly.calendar.domain.service.FriendService
 import com.friendly.calendar.domain.service.FriendStatusService
+import com.friendly.calendar.domain.service.UserService
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.shouldBe
@@ -13,9 +16,15 @@ import java.util.Optional
 
 class FriendStatusServiceTest : AnnotationSpec() {
 
-    private val userRepository = mockk<UserRepository>()
+    private val userService = mockk<UserService>()
 
     private val friendRequestRepository = mockk<FriendRequestRepository>()
+
+    private val friendRelationRepository = mockk<FriendRelationRepository>()
+
+    private val friendService = mockk<FriendService>()
+
+    private val applicationEventPublisher = mockk<org.springframework.context.ApplicationEventPublisher>()
 
     private val existsUserId1: Long = 1
     private val existsUserId2: Long = 2
@@ -23,48 +32,44 @@ class FriendStatusServiceTest : AnnotationSpec() {
 
     @BeforeEach
     fun setUp() {
-        every { userRepository.findById(existsUserId1) } returns Optional.of(
-            User(
-                id = 1,
-                username = "jylim",
-                password = "jylim12345",
-                email = "jylim@test.com",
-                phoneNumber = "010-1234-5678",
-                name = "TestName",
-            )
+        every { userService.findUserById(existsUserId1) } returns User(
+            id = 1,
+            username = "jylim",
+            password = "jylim12345",
+            email = "jylim@test.com",
+            phoneNumber = "010-1234-5678",
+            name = "TestName",
         )
 
-        every { userRepository.findById(existsUserId2) } returns Optional.of(
-            User(
-                id = 2,
-                username = "jylim2",
-                password = "jylim12345",
-                email = "limjy@test.com",
-                phoneNumber = "010-1111-2222",
-                name = "TestName2",
-            )
+        every { userService.findUserById(existsUserId2) } returns User(
+            id = 2,
+            username = "jylim2",
+            password = "jylim12345",
+            email = "limjy@test.com",
+            phoneNumber = "010-1111-2222",
+            name = "TestName2",
         )
 
-        every { userRepository.findById(notExistsUserId) } returns Optional.empty()
+        every { userService.findUserById(notExistsUserId) } returns throw IllegalArgumentException("User with id $notExistsUserId not found")
 
         every { friendRequestRepository.save(any()) } returns mockk()
     }
 
     @Test
     fun `Should fail when sender is missing`() {
-        val friendStatusService = FriendStatusService(friendRequestRepository, userRepository)
+        val friendStatusService = FriendStatusService(friendRequestRepository, friendRelationRepository, friendService, userService, applicationEventPublisher)
 
         shouldThrow<IllegalArgumentException> {
             friendStatusService.requestFriend(notExistsUserId, existsUserId2, "testMessage")
-        }.message shouldBe "sender not found"
+        }
     }
 
     @Test
     fun `Should fail when receiver is missing`() {
-        val friendStatusService = FriendStatusService(friendRequestRepository, userRepository)
+        val friendStatusService = FriendStatusService(friendRequestRepository, friendRelationRepository, friendService, userService, applicationEventPublisher)
 
         shouldThrow<IllegalArgumentException> {
             friendStatusService.requestFriend(existsUserId1, notExistsUserId, "testMessage")
-        }.message shouldBe "receiver not found"
+        }
     }
 }
