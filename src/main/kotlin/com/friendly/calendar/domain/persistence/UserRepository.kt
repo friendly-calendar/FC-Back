da39325efbdd39781b94cb8af72bc66af2ad9bd2
dokdogalmaegi <dokdogalmaegi@gmail.com>
2023-12-01 12:14:40 +0900

    delete User

--- src/main/kotlin/com/friendly/calendar/domain/persistence/UserRepository.kt
+++ src/main/kotlin/com/friendly/calendar/domain/persistence/UserRepository.kt
@@ -1,11 +1,10 @@
 package com.friendly.calendar.domain.persistence
 
 import com.friendly.calendar.domain.model.CalendarUser
-import com.friendly.calendar.domain.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : JpaRepository<User, Long> {
+interface UserRepository : JpaRepository<CalendarUser, Long> {
     fun findByUsername(username: String): CalendarUser?
 }
