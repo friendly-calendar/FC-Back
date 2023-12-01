eb8377a5b17aa5633c3b6812031fcb8f503739f1
dokdogalmaegi <clik202012@gmail.com>
2023-12-01 19:38:39 +0900

    Add ResponseDTO and UserController

---
+++ src/main/kotlin/com/friendly/calendar/controller/UserController.kt
@@ -0,0 +1,20 @@
+package com.friendly.calendar.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/users", produces = [MediaType.APPLICATION_JSON_VALUE])
+class UserController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun signUp() {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ src/main/kotlin/com/friendly/calendar/network/ResponseDTO.kt
@@ -0,0 +1,16 @@
+package com.friendly.calendar.network
+
+import org.springframework.http.HttpStatus
+import java.time.LocalDateTime
+
+data class ResponseDTO(
+    val code: Int,
+    val description: String,
+    val data: Any?,
+    val responseTime: String = LocalDateTime.now().toString(),
+) {
+    companion object {
+        fun ok(description: String = "OK", data: Any? = null) = ResponseDTO(HttpStatus.OK.value(), description, data)
+        fun error(code: Int, description: String, data: Any? = null) = ResponseDTO(code, description, null)
+    }
+}
