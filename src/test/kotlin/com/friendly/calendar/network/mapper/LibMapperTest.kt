341920ebd00c001da0422c5635ef6beaee3bc2fe
dokdogalmaegi <clik202012@gmail.com>
2023-12-02 15:03:34 +0900

    library mapper test

--- src/test/kotlin/com/friendly/calendar/network/mapper/LibMapperTest.kt
+++ src/test/kotlin/com/friendly/calendar/network/mapper/LibMapperTest.kt
@@ -1,32 +1,86 @@
 package com.friendly.calendar.network.mapper
 
-import com.friendly.calendar.domain.model.CalendarUser
-import com.friendly.calendar.network.UserSignUpDTO
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.modelmapper.ModelMapper
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 
 class LibMapperTest {
 
+    private lateinit var fromDTO: PublicTestDTO
+
+    private val modelMapper = ModelMapper()
+
+    private val name = "dokdogal"
+    private val email = "dokdogalmaegi@gmail.com"
+    private val username = "dokdogalmaegi"
+    private val password = "password123!"
+    private val phoneNumber = "010-1234-5678"
+
+    @BeforeEach
+    fun setUp() {
+        fromDTO = PublicTestDTO(
+            name,
+            email,
+            username,
+            password,
+            phoneNumber
+        )
+    }
+
     @Test
-    fun map() {
-        val validUserSignUpDTO = UserSignUpDTO(
-            name = "dokdogal",
-            email = "dokdogalmaegi@gmail.com",
-            username = "dokdogalmaegi",
-            password = "password123!",
-            phoneNumber = "010-1234-5678"
+    fun `public property를 변환할 수 있다`() {
+        val testEntity = modelMapper.map(fromDTO, PublicEntity::class.java)
+        assertAll(
+            { assertThat(testEntity.username).isEqualTo(username) },
+            { assertThat(testEntity.name).isEqualTo(name) },
+            { assertThat(testEntity.email).isEqualTo(email) },
+            { assertThat(testEntity.password).isEqualTo(password) },
+            { assertThat(testEntity.phoneNumber).isEqualTo(phoneNumber) }
+        )
+    }
+
+    @Test
+    fun `private property를 변환할 수 없다`() {
+        val testEntity = modelMapper.map(fromDTO, PrivateEntity::class.java)
+        assertAll(
+            { assertThat(testEntity.username).isNotEqualTo(username).isEmpty() },
+            { assertThat(testEntity.name).isNotEqualTo(name).isEmpty() },
+            { assertThat(testEntity.email).isNotEqualTo(email).isEmpty() },
+            { assertThat(testEntity.password).isNotEqualTo(password).isEmpty() },
+            { assertThat(testEntity.phoneNumber).isNotEqualTo(phoneNumber).isEmpty() }
+        )
+    }
+
+    companion object {
+        data class PublicTestDTO(
+            var name: String,
+            var email: String,
+            var username: String,
+            var password: String,
+            var phoneNumber: String
         )
 
-        val modelMapper = ModelMapper()
-        val calendarUser = modelMapper.map(validUserSignUpDTO, CalendarUser::class.java)
-        Assertions.assertThat(calendarUser.username).isEqualTo("dokdogalmaegi")
-        Assertions.assertThat(calendarUser?.name).isEqualTo("dokdogal")
-        Assertions.assertThat(calendarUser?.email).isEqualTo("dokdogalmaegi@gmail.com")
-        Assertions.assertThat(calendarUser?.password).isEqualTo("password123!")
-        Assertions.assertThat(calendarUser?.phoneNumber).isEqualTo("010-1234-5678")
+        class PublicEntity() {
+            var name: String = ""
+            var email: String = ""
+            var username: String = ""
+            var password: String = ""
+            var phoneNumber: String = ""
+        }
+
+        class PrivateEntity() {
+            var name: String = ""
+                private set
+            var email: String = ""
+                private set
+            var username: String = ""
+                private set
+            var password: String = ""
+                private set
+            var phoneNumber: String = ""
+                private set
+        }
     }
 }
