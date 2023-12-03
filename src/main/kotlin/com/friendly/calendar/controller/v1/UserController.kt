5ebf7f5b3b0005e0915c23b58bfe6c5740579fd6
dokdogalmaegi <clik202012@gmail.com>
2023-12-03 20:11:30 +0900

    me controller 추가

--- src/main/kotlin/com/friendly/calendar/controller/v1/UserController.kt
+++ src/main/kotlin/com/friendly/calendar/controller/v1/UserController.kt
@@ -2,11 +2,14 @@ package com.friendly.calendar.controller.v1
 
 import com.friendly.calendar.domain.service.UserService
 import com.friendly.calendar.network.ResponseDTO
+import com.friendly.calendar.network.UserDTO
 import com.friendly.calendar.network.UserSignUpDTO
+import com.friendly.calendar.security.session.CalendarPrincipal
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -27,4 +30,22 @@ class UserController(
     @GetMapping
     @PreAuthorize("isAuthenticated() && hasRole('ROLE_ADMIN')")
     fun getUsers() = ResponseDTO.ok(data = userService.getUsers())
+
+    @GetMapping("/me")
+    @PreAuthorize("isAuthenticated()")
+    fun getMyInfo(authentication: Authentication): ResponseDTO {
+        val calendarPrincipal = authentication.principal as CalendarPrincipal
+        val userDTO = calendarPrincipal.user.let {
+            UserDTO(
+                id = it.id,
+                username = it.username,
+                roles = it.roles.toList(),
+                email = it.email,
+                name = it.name,
+                phoneNumber = it.phoneNumber
+            )
+        }
+
+        return ResponseDTO.ok(data = userDTO)
+    }
 }
