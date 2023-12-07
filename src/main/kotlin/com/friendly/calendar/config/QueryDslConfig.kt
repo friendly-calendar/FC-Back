2e353c66908658e20790895caa5f17bdd2e0bc41
dokdogalmaegi <clik202012@gmail.com>
2023-12-07 19:08:20 +0900

    Add queryDSL

---
+++ src/main/kotlin/com/friendly/calendar/config/QueryDslConfig.kt
@@ -0,0 +1,17 @@
+package com.friendly.calendar.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+
+@Configuration
+class QueryDslConfig {
+
+    @PersistenceContext
+    lateinit var em: EntityManager
+
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory = JPAQueryFactory(em)
+}
