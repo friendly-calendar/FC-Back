0206c21450364fba3d471fedcb2c8e06b7e739dc
dokdogalmaegi <clik202012@gmail.com>
2023-12-28 09:05:08 +0900

    불필요 주입 제거

--- src/test/kotlin/com/friendly/calendar/controller/v1/annoationimpl/WithMockCalendarUserSecurityContextFactory.kt
+++ src/test/kotlin/com/friendly/calendar/controller/v1/annoationimpl/WithMockCalendarUserSecurityContextFactory.kt
@@ -2,17 +2,13 @@ package com.friendly.calendar.controller.v1.annoationimpl
 
 import com.friendly.calendar.controller.v1.testannotation.WithMockCalendarUser
 import com.friendly.calendar.domain.model.CalendarUser
-import com.friendly.calendar.domain.persistence.CalendarUserRepository
 import com.friendly.calendar.security.session.CalendarPrincipal
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.test.context.support.WithSecurityContextFactory
 
-class WithMockCalendarUserSecurityContextFactory @Autowired constructor(
-    private val calendarUserRepository: CalendarUserRepository
-) : WithSecurityContextFactory<WithMockCalendarUser> {
+class WithMockCalendarUserSecurityContextFactory : WithSecurityContextFactory<WithMockCalendarUser> {
     override fun createSecurityContext(testCalendarUser: WithMockCalendarUser): SecurityContext {
         val context = SecurityContextHolder.createEmptyContext()
 
